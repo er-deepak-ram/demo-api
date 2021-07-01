dc976ec3957e8dd5215fa38aa296271b17227f34
Deepak Kumar Ram <deepak.kumar2305@gmail.com>
2021-07-01 07:48:33 +0530

    DTO added

---
+++ src/main/java/com/deepak/demoapi/dto/Car.java
@@ -0,0 +1,15 @@
+package com.deepak.demoapi.dto;
+
+import lombok.Data;
+
+@Data
+public class Car {
+
+	private int id;
+	
+	private String name;
+	
+	private String companyName;
+	
+	private Double price;
+}
